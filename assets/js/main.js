a67e74bb479ffcbbf45c6d8afe648c100ba5d954
Swan Yee Tun <swanyeetun.2000@gmail.com>
2021-06-30 18:26:26 +0630

    final stage

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // swiper js for discover section
 
-      var swiper = new Swiper(".discover-swiper", {
+      var swiper = new Swiper(".swiper-container", {
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
@@ -78,3 +78,48 @@ function finalVideo() {
 videoFile.addEventListener("ended",finalVideo)
   
 videoButton.addEventListener('click',playPause)
+
+// show scroll up
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+
+  // scroll will show when reach next section
+  if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
+
+// scroll change backgroud of nav
+function scrollHeader() {
+  const scrollHeader = document.getElementById("scroll-header");
+
+  if (this.scrollY > 100) {
+    scrollHeader.classList.add('scrolled-header');
+    scrollHeader.classList.remove('nav-bg-color');
+  } else {
+    scrollHeader.classList.remove("scrolled-header");
+    scrollHeader.classList.add('nav-bg-color');
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+
+// scroll up active
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
